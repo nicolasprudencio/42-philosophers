fd4833df8377bd179d8f291f8ae7d34cdfe1e7e6
nicolasprudencio <nicolas-prudencio@hotmail.com>
2024-04-02 21:36:23 -0300

    adding new functions and int_max macro

--- philo/philos.h
+++ philo/philos.h
@@ -10,19 +10,22 @@
 
 # define TRUE 1
 # define FALSE 0
+# define INT_MAX 2147483647
 
 typedef struct s_philos {
 	int				id;
-	pthread_t 		tr;
-	pthread_mutex_t *fork_rt;
-	pthread_mutex_t fork_lf;
-	pthread_t		th;
 	int				n;
 	int				last_meal;
 	int				m_counter;
+	pthread_t 		tr;
+	pthread_t		th;
+	struct s_data	*info;
+	pthread_mutex_t *fork_rt;
+	pthread_mutex_t fork_lf;
 }				t_philos;
 
 typedef struct s_data {
+	pthread_mutex_t principal;
 	pthread_mutex_t	m_print;
 	pthread_mutex_t	m_dead;
 	int				n_philos;
@@ -31,6 +34,7 @@ typedef struct s_data {
 	int				t_to_sleep;
 	int				t_to_die;
 	int				n_of_meals;
+	long int		start;
 	t_philos		*philos;
 }					t_data;
 
@@ -39,7 +43,9 @@ typedef struct s_data {
 
 void	init_variables(t_data *data, char **argv);
 int		init_philos(t_philos *philo, t_data *data);
+void	*monitor(void *philos);
 void	*life_cycle(void *philos);
+void	take_forks(t_philos *philo);
 
 // UTILS
 
@@ -48,5 +54,9 @@ int			ft_isnbr(char	*nptr);
 int			ft_atoi(const char *nptr);
 long int	ft_atol(char *nptr);
 int			ft_isdigit(int c);
+long int	get_time(void);
+void		ft_usleep(int ms);
+int			is_dead(t_philos *philo, int	flag);
+void		print(t_philos *philo, char *s);
 
 #endif

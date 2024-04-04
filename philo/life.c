a724e8bf2009a048b64ae42634dd8751e013b35a
nicolasprudencio <nicolas-prudencio@hotmail.com>
2024-04-04 20:36:21 -0300

    adding new actions

--- philo/life.c
+++ philo/life.c
@@ -6,66 +6,76 @@
 /*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 18:57:47 by nprudenc          #+#    #+#             */
-/*   Updated: 2024/04/02 21:33:59 by nprudenc         ###   ########.fr       */
+/*   Updated: 2024/04/04 20:30:46 by nprudenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philos.h"
 
-void	*monitor(void *philos)
+void take_forks(t_philos *philo)
 {
-	t_philos *philo;
-
-	philo = philos;
-	pthread_mutex_lock(&philo->info->m_print);
-	printf("montiring");
-	pthread_mutex_unlock(&philo->info->m_print);
-	return (NULL);
-}
-
-void	take_forks(t_philos	*philo)
-{
-	//possible data racing happening
-	pthread_mutex_lock(&philo->fork_lf);
-	print(philo, "has taken left fork\n");
-	if (philo->info->n_philos == 1)
+	pthread_mutex_t *first_fork;
+	pthread_mutex_t *second_fork;
+	
+	pthread_mutex_lock(&philo->info->m_principal);
+	if (philo->id % 2 != 0)
 	{
-		ft_usleep(philo->info->t_to_die * 2);
-		return ;
+		first_fork = &philo->fork_lf;
+		second_fork = philo->fork_rt;
 	}
-	pthread_mutex_lock(philo->fork_rt);
-	print(philo, "has taken right fork\n");
+	else
+	{
+		first_fork = philo->fork_rt;
+		second_fork = &philo->fork_lf;
+	}
+	pthread_mutex_unlock(&philo->info->m_principal);
+	pthread_mutex_lock(first_fork);
+	print(philo, "has taken first fork\n");
+	pthread_mutex_lock(second_fork);
+	print(philo, "has taken second fork\n");
 }
 
-void	eat(t_philos *philo)
+void	ph_eat(t_philos *philo)
 {
 	print(philo, "is eating\n");
+	ft_usleep(philo->info->t_to_eat);
+	philo->m_counter++;
+	philo->last_meal = get_time();
 	pthread_mutex_unlock(&philo->fork_lf);
 	print(philo, "has dropped the left fork\n");
 	pthread_mutex_unlock(philo->fork_rt);
 	print(philo, "has dropped the right fork\n");
-	ft_usleep(philo->info->t_to_eat);
+}
+
+void	ph_sleep(t_philos *philo)
+{
+	if (is_dead(philo, 0))
+		return ;
+	print(philo, "is sleeping\n");	
+	ft_usleep(philo->info->t_to_sleep);
+}
+
+void	ph_think(t_philos *philo)
+{
+	if (is_dead(philo, 0))
+		return ;
+	print(philo, "is thinking\n");
 }
 
 void	*life_cycle(void *philos)
 {
 	t_philos *philo;
-	// pthread_t	m;
+	int	i = 10;
 
 	philo = philos;
-	if (philo->id % 2 == 0)
-	{
-		// printf("entering\n");
-		ft_usleep(philo->info->t_to_eat / 10);
-		// printf("exiting\n");
-	}
-	// if (pthread_create(&m, NULL, &monitor, philos) != 0)
-	// 	return (NULL);
-	// pthread_detach(m);
+	// if (philo->id % 2 == 0)
+	// 	ft_usleep(philo->info->t_to_eat / 10);
 	while (!is_dead(philo, 0))
 	{
 		take_forks(philo);
-		eat(philo);
+		ph_eat(philo);
+		ph_sleep(philo);
+		ph_think(philo);
 	}
 	return (NULL);
 }

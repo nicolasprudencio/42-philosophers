72927bbca38f1bc7a286921ce696773a62ee96c1
nicolasprudencio <nicolas-prudencio@hotmail.com>
2024-04-04 20:34:58 -0300

    applying conditionals at print function

--- philo/utils/utils.c
+++ philo/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 17:52:59 by nprudenc          #+#    #+#             */
-/*   Updated: 2024/04/02 20:36:17 by nprudenc         ###   ########.fr       */
+/*   Updated: 2024/04/04 20:33:57 by nprudenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,21 +35,22 @@ void	print(t_philos *philo, char *s)
 	long int	curr_time;
 
 	pthread_mutex_lock(&philo->info->m_print);
-	time = get_time() - philo->info->start;
-	if (time >= 0 && time <= INT_MAX && !is_dead(philo, 0))
+	time = get_time() - philo->info->start_time;
+	if (time >= 0 && time <= INT_MAX && !is_dead(philo, 0) && \
+	philo->m_counter != philo->info->n_of_meals)
 	{
-		curr_time = get_time() - philo->info->start;
+		curr_time = get_time() - philo->info->start_time;
 		printf("%ld %d %s", curr_time, philo->id, s);
 	}
 	pthread_mutex_unlock(&philo->info->m_print);
 }
 
-int	is_dead(t_philos *philo, int	flag)
+int	is_dead(t_philos *philo, int flag)
 {
 	static int	dead = 0;
 
 	pthread_mutex_lock(&philo->info->m_dead);
-	if (flag != 0)
+	if (flag)
 		dead = flag;
 	pthread_mutex_unlock(&philo->info->m_dead);
 	return (dead);
